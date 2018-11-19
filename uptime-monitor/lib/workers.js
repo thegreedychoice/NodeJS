c542945a92977c4ace53bf5b3128f71a9623679b
Shubham Shukla <shubham.shukla77@hotmail.com>
2018-11-18 20:59:03 -0500

    Added background workers to monitor checks

--- uptime-monitor/lib/workers.js
+++ uptime-monitor/lib/workers.js
@@ -15,12 +15,181 @@ var url = require('url');
 //Instantiate the worker object
 var workers = {};
 
-//Timer to execute the woker-process once per minute 
+//Lookup all checks, get their data, and send to a vaidator
+workers.gatherAllChecks = function(){
+  //Get all the checks
+  _data.list('checks', function(err, checks){
+    if(!err && checks && checks.length > 0){
+      checks.forEach(function(check){
+        //Read in the check data
+        _data.read('checks', check, function(err, originalCheckData){
+          if(!err && originalCheckData){
+            //Pass it to the check validator, and let that function log or continue as needed
+            workers.validateCheckData(originalCheckData);
+          } else {
+            console.log('Error reading one of the check\'s data');
+          }
+        });
+      });
+    } else {
+      console.log('Error: Could not find any checks to process');
+    }
+  });
+};
+
+//Sanity-check the check data
+workers.validateCheckData = function(originalCheckData){
+  originalCheckData = typeof(originalCheckData) == 'object' && originalCheckData !== null ? originalCheckData : {};
+  originalCheckData.id = typeof(originalCheckData.id) == 'string' && originalCheckData.id.trim().length == 20 ? originalCheckData.id.trim() : false;
+  originalCheckData.userPhone = typeof(originalCheckData.userPhone) == 'string' && originalCheckData.userPhone.trim().length == 10 ? originalCheckData.userPhone.trim() : false;
+  originalCheckData.protocol = typeof(originalCheckData.protocol) == 'string' && ['http', 'https'].indexOf(originalCheckData.protocol) > -1 ? originalCheckData.protocol : false;
+  originalCheckData.url = typeof(originalCheckData.url) == 'string' && originalCheckData.url.trim().length > 0 ? originalCheckData.url.trim() : false;
+  originalCheckData.method = typeof(originalCheckData.method) == 'string' && ['get', 'post', 'put', 'delete'].indexOf(originalCheckData.method) > -1 ? originalCheckData.method : false;
+  originalCheckData.successCodes = typeof(originalCheckData.successCodes) == 'object' && originalCheckData.successCodes instanceof Array && originalCheckData.successCodes.length > 0 ? originalCheckData.successCodes : false;
+  originalCheckData.timeoutSeconds = typeof(originalCheckData.timeoutSeconds) == 'number' && originalCheckData.timeoutSeconds % 1 === 0 && originalCheckData.timeoutSeconds >= 1 && originalCheckData.timeoutSeconds <= 5 ? originalCheckData.timeoutSeconds : false;
+
+  //Set the keys if not have been set (if the workers have never seen this check before)
+  originalCheckData.state = typeof(originalCheckData.state) == 'string' && ['up', 'down'].indexOf(originalCheckData.state) > -1 ? originalCheckData.state : 'down';
+  originalCheckData.lastChecked = typeof(originalCheckData.lastChecked) == 'number' && originalCheckData.lastChecked > 0  ? originalCheckData.lastChecked : false;
+
+  //if all the checks passed, pass the data along to the next step of the process
+  if(originalCheckData.id &&
+    originalCheckData.userPhone &&
+    originalCheckData.protocol &&
+    originalCheckData.url &&
+    originalCheckData.method &&
+    originalCheckData.successCodes &&
+    originalCheckData.timeoutSeconds){
+      workers.performCheck(originalCheckData);
+    } else {
+      console.log('Error: One of the checks is not properly formatted. Skipping it');
+    }
+};
+
+//Perform the check, send the original checkdata and the outcome of the check process, to the next step in the process
+workers.performCheck = function(originalCheckData){
+  //Prepare the initial check outcome
+  var checkOutcome = {
+    'error': false,
+    'responseCode': false
+  };
+
+  //Mark that the outcome has not been sent yet
+  var outcomeSent = false;
+
+  //Parse the hostname and the path out of the original check data
+  var parsedUrl = url.parse(originalCheckData.protocol+'://'+originalCheckData.url, true);
+  var hostName = parsedUrl.hostname;
+  var path = parsedUrl.path //Using path & not pathname, since we want teh query string
+
+  //Construct the request
+  var requestDetails = {
+    'protocol': originalCheckData.protocol+':',
+    'hostname': hostName,
+    'method': originalCheckData.method.toUpperCase(),
+    'path': path,
+    'timeout': originalCheckData.timeoutSeconds * 1000
+  };
+
+  //Instantiate the request object (using either http or https module)
+  var _moduleToUse = originalCheckData.protocol == 'http' ? http : https;
+  var req = _moduleToUse.request(requestDetails, function(res){
+    //grab the status of the request sent
+    var status = res.statusCode;
+
+    //Update the checkOutcome and pass the data along
+    checkOutcome.responseCode = status;
+    if(!outcomeSent){
+      workers.processCheckOutcome(originalCheckData, checkOutcome);
+      outcomeSent = true;
+    }
+  });
+
+  //Bind to the error event so it doesn't get thrown
+  req.on('error', function(e){
+    //Update the checkOutcome and pass the data along
+    checkOutcome.error = {
+      'error': true,
+      'value': e
+    };
+    if(!outcomeSent){
+      workers.processCheckOutcome(originalCheckData, checkOutcome);
+      outcomeSent = true;
+    }
+  });
+
+  //Bind to the timeout Event
+  req.on('timeout', function(e){
+    //Update the checkOutcome and pass the data along
+    checkOutcome.error = {
+      'error': true,
+      'value': 'timeout'
+    };
+    if(!outcomeSent){
+      workers.processCheckOutcome(originalCheckData, checkOutcome);
+      outcomeSent = true;
+    }
+  });
+
+  //End the request
+  req.end();
+};
+
+//Process the check outcome and, update the check data as needed, trigger an alert if needed
+
+//Special logic for accomodating a check that has never been tested before (don't alert on that one)
+
+workers.processCheckOutcome = function(originalCheckData, checkOutcome){
+  //Decide if the check is considered up or down
+  var state = !checkOutcome.error && checkOutcome.responseCode && originalCheckData.successCodes.indexOf(checkOutcome.responseCode) > -1 ? 'up' : 'down';
+
+  //Decide if an alert is warranted
+  var alertWarranted = originalCheckData.lastChecked && originalCheckData.state !== state ? true : false;
+
+  //Update the check data
+  var newCheckData = originalCheckData;
+  newCheckData.state = state;
+  newCheckData.lastChecked = Date.now();
+
+  //Save the updates to the disk
+  _data.update('checks', newCheckData.id, newCheckData, function(err){
+    //console.log(err);
+    if(!err){
+      //Send the new check data to the next phase in the process if needed
+      if(alertWarranted){
+        workers.alertUsertoStatusChange(newCheckData);
+      } else {
+        console.log('Check outcome has not changed, no alert needed');
+      }
+    } else {
+      console.log('Error : Trying to save updates to one of the checks');
+    }
+  })
+};
+
+//Alert the user as to a change in their check status
+workers.alertUsertoStatusChange = function(newCheckData){
+  var msg = 'Alert: Your check for '+newCheckData.method.toUpperCase()+' '+newCheckData.protocol+'://'+newCheckData.url+' is currently '+newCheckData.state;
+  helpers.sendTwilioSms(newCheckData.userPhone, msg, function(err){
+    if(!err){
+      console.log("Success: User was alerted to the status change in their check, via SMS: ", msg);
+    } else {
+      console.log('Error: Could not send SMS to user to inform status change in check');
+    }
+  });
+};
+
+//Timer to execute the woker-process once per minute
+workers.loop = function(){
+  setInterval(function(){
+    workers.gatherAllChecks();
+  }, 1000 * 5)
+}
 
 //Init Script
 workers.init = function(){
   //Execute all the checks immediately
-  worker.gatherAllChecks();
+  workers.gatherAllChecks();
 
   //Call the loop so the checks will execute later on
   workers.loop();
